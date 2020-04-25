721e8c4dd1f9bd37db118e869aa3e6360abe4d81
norbertomagarzo <norbertomagarzo@gmail.com>
2020-04-25 18:09:58 -0300

    booarrado

--- src/main/java/com/mobydigital/keysapp/backend/app/models/dao/IKeyDao.java
+++
@@ -1,9 +0,0 @@
-package com.mobydigital.keysapp.backend.app.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.mobydigital.keysapp.backend.app.models.entity.Key;
-
-public interface IKeyDao extends CrudRepository<Key, Integer> {
-
-}
