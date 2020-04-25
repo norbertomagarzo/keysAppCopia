1378f04ce4aaf8815ce687ab8032e8b5372196dd
norbertomagarzo <norbertomagarzo@gmail.com>
2020-04-25 17:53:23 -0300

    package

--- client/server.js
+++ client/server.js
@@ -1,14 +1,16 @@
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
+ 
 const app = express();
-
-// Serve static files
-app.use(express.static(__dirname + '/dist/keys-app-mobydigital'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/keys-app-mobydigital/index.html'));
+ 
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/keysAppCopia'));
+ 
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/keysAppCopia/index.html'));
 });
-
-// default Heroku port
-app.listen(process.env.PORT || 4200);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
